d05e411cb8ca20bc5345c93920fd3bfc3d4151e9
Daniel Gretarsson <danielgretars@gmail.com>
2021-11-05 17:48:54 +0000

    More robust creation of virtual MIDI ports on Windows
    It provess to be more stable to wait some ms before using a virtual MIDI port after creating it.

--- modules/juce_audio_devices/native/juce_win32_Midi.cpp
+++ modules/juce_audio_devices/native/juce_win32_Midi.cpp
@@ -2052,11 +2052,19 @@ Win32VirtualMidi& getOrCreatePort(const String& name)
 {
     const auto[it, was_inserted] = virtualMidiPorts.emplace(name, name);
 
-    DBG("getOrCreatePort() " << name << ", was created? " << (was_inserted ? "yes" : "no"));
-
-    ignoreUnused(was_inserted);
+    if (was_inserted)
+    {
+        // It seems to be more reliable to wait a few ms before using the port after creating it.
+        // Otherwise, the JUCE MIDI enumeration doesn't always pick up the ports
+        Thread::sleep(20);
+    }
 
     return it->second;
+}
+
+void releasePort(Win32VirtualMidi& port)
+{
+    virtualMidiPorts.erase(virtualMidiPorts.find(port.getName()));
 }
 
 //======================================================================================================================
@@ -2066,12 +2074,17 @@ struct Win32VirtualMidiOutput : public MidiOutput::Pimpl
             : output(out),
               port(getOrCreatePort(out.getName()))
     {
+        DBG("Create virtual MIDI output: " << getDeviceName());
+
         port.startTx();
     }
 
     ~Win32VirtualMidiOutput() override
     {
         port.stopTx();
+
+        if (!port.isBeingUsed())
+            releasePort(port);
     }
 
     String getDeviceIdentifier() override { return output.getIdentifier(); }
@@ -2106,8 +2119,6 @@ struct Win32VirtualMidiInput : public MidiInput::Pimpl
 
         port.startRx([this](const uint8_t* bytes, size_t length)
         {
-            DBG("handleMidiData " << length);
-
             callback->handleIncomingMidiMessage(&input, juce::MidiMessage(bytes, length));
         });
     }
@@ -2115,6 +2126,9 @@ struct Win32VirtualMidiInput : public MidiInput::Pimpl
     ~Win32VirtualMidiInput() override
     {
         port.stopRx();
+
+        if (!port.isBeingUsed())
+            releasePort(port);
     }
 
     String getDeviceIdentifier() override { return input.getIdentifier(); }
